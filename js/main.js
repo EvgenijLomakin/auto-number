064a7f1fce91b3bc5406094beda6e96746be3a7b
EvgenijLomakin <48293882+EvgenijLomakin@users.noreply.github.com>
2025-06-02 18:06:14 +0300

    Update main.js новые данные

--- js/main.js
+++ js/main.js
@@ -1,123 +1,305 @@
+console.log('auto-namber main.js')
+let local_car_nambers, local_car_parking_by_time;
+let local_car_nambers_entries, local_car_parking_by_time_entries;
+let all_cars = {
+	'all' : {},
+	'car_nambers' : {},
+	'car_parking_by_time' : {}
+};
 let tab__button_wrapper = document.querySelector('.tab__button-wrapper');
 let tab_btns = tab__button_wrapper.querySelectorAll('.tab__button');
 let tab_btns_len = tab_btns.length;
 let tab__container_wrapper = document.querySelector('.tab__container-wrapper'); 
 let tab__containers = tab__container_wrapper.querySelectorAll('.tab__container');
 let tab__containers_len = tab__containers.length;
-let list_car_namber = tab__container_wrapper.querySelector('.tab__container[data-name="list-car-namber"]');
-let count_all_cars = 0;
-let car_search__form = document.querySelector('.car-search__form');
-let car_search__out_car_number = car_search__form.querySelector('.car-search__out-car-number');
-let car_search__input = car_search__form.querySelector('.car-search__input');
-let car_search__input_value;
-let empty_number = false;
-let input_car_interactive = document.querySelector('#search-car-number');
-let input_car_interactive_number;
-let car_search_out_text = document.querySelector('.car-search__out-text');
 
-list_car_namber.innerHTML = '';
+let tab__container_all = tab__container_wrapper.querySelector('.tab__container[data-name="all"]');
+let tab__container_parking_by_time = tab__container_wrapper.querySelector('.tab__container[data-name="parking_by_time"]');
+let tab__container_car_all = tab__container_wrapper.querySelector('.tab__container[data-name="all"]');
+let tab__container_car_numbers = tab__container_wrapper.querySelector('.tab__container[data-name="car_nambers"]')
+let table_body_search = document.querySelector('.table-body-search');
+let search_form = document.querySelector('.form-table-search');
 
-for ( let i = 0; i < car_nambers.length; i++ )
+
+// установить локол сторыж, если ещё не учтановлен
+// для car_nambers
+if ( localStorage.getItem('car_nambers') == null )
 {
-	list_car_namber.innerHTML += `<details class="details" data-namber="${i}" name="auto-number">
-	<summary class="details__summary"><span>${i}</span></summary>
-	<div class="details__content" data-namber="${i}"></div><!-- .details__content --></details>`;
+	// console.log('car_nambers localStorage - false')
+	// console.log('car_nambers js file')
+	// console.dir(car_nambers)
+	localStorage.setItem('car_nambers', JSON.stringify( car_nambers ) );
 }
-for ( let i = 0; i < car_nambers.length; i++ )
+// для car_parking_by_time
+if ( localStorage.getItem('car_parking_by_time') == null )
 {
-	car_nambers[i].sort( ( a, b ) => a - b  );
-	let details__content = list_car_namber.querySelector(`.details__content[data-namber="${i}"]`);
-	for ( y = 0; y < car_nambers[i].length; y++ )
+	// console.log('car_parking_by_time localStorage - false')
+	// console.log('car_parking_by_time js file')
+	// console.dir(car_parking_by_time)
+	localStorage.setItem('car_parking_by_time', JSON.stringify( car_parking_by_time ) );
+}
+
+local_car_nambers = JSON.parse( localStorage.getItem('car_nambers') );
+local_car_parking_by_time = JSON.parse( localStorage.getItem('car_parking_by_time') );
+
+// console.log('local_car_nambers')
+// console.dir(local_car_nambers)
+// console.log('local_car_parking_by_time')
+// console.dir(local_car_parking_by_time)
+local_car_nambers_entries = Object.entries(local_car_nambers);
+local_car_parking_by_time_entries = Object.entries(local_car_parking_by_time);
+// console.dir(local_car_nambers_entries )
+// console.dir(local_car_parking_by_time_entries )
+
+// заполняем общий [ all_cars.all ] обьект ( car_nambers + car_parking_by_time )
+for ( let i = 0; i < local_car_nambers_entries.length; i++ )
+{
+	//all_cars.car_nambers[local_car_nambers_entries[i][0]] = local_car_nambers_entries[i][1];
+	all_cars.all[local_car_nambers_entries[i][0]] = local_car_nambers_entries[i][1];
+}
+
+for ( let i = 0; i < local_car_parking_by_time_entries.length; i++ )
+{
+	// local_car_nambers_entries
+	all_cars.car_parking_by_time[local_car_parking_by_time_entries[i][0]] = local_car_parking_by_time_entries[i][1];
+}
+for ( let i = 0; i < local_car_nambers_entries.length; i++ )
+{
+	// console.log('all_cars.car_parking_by_time[i] ')
+	// console.dir(all_cars.car_parking_by_time[i] )
+	Object.assign(all_cars.all[i], all_cars.car_parking_by_time[i] );
+}
+
+
+console.log('all_cars')
+console.dir(all_cars)
+render_car_all( all_cars.all, tab__container_car_all );
+render_car_numbers( all_cars.car_nambers, tab__container_car_numbers );
+render_car_parking_by_time( all_cars.car_parking_by_time, tab__container_parking_by_time );
+render_table_search( all_cars.all, table_body_search );
+search_nubber( search_form, table_body_search );
+// Render
+function render_car_all( car, container )
+{
+	console.log('render_car_all( car, container )')
+	let car_entries = Object.entries(car);
+	let car_entries_length = car_entries.length;
+	let all_cout = 0;
+	let this_details, this_obj_entries, this_obj_entries_len;
+	let details__content;
+	container.innerHTML = '';
+	for ( let i = 0; i < car_entries_length; i++ )
 	{
-		
-		if ( (car_nambers[i][y] < 10) )
+		// console.log('---')
+		// console.log('Number : '+ car_entries[i][0] )
+		// console.dir(car_entries[i][1])
+		container.innerHTML += `<details class="details" name="all" data-number="${car_entries[i][0]}">
+		<summary class="details__summary">
+		<span>${car_entries[i][0]}</span></summary>
+		<div class="details__content" data-namber="${car_entries[i][0]}"></div>
+		</details>`;
+	}
+	for ( let i = 0; i < car_entries_length; i++ )
+	{
+		this_details =  container.querySelector(`details[data-number="${car_entries[i][0]}"]`);
+		details__content = this_details.querySelector('.details__content');
+		this_obj_entries = Object.entries(car_entries[i][1]);
+		this_obj_entries_len = this_obj_entries.length;
+		details__content.innerHTML = '';
+		let item_count = 0;
+		for ( let y = 0; y < this_obj_entries_len; y++ )
 		{
-			details__content.innerHTML += `<p class="details__content_p"><span>00${car_nambers[i][y]}</span></p>`;
+			item_count++;
+			all_cout++;
+			if ( this_obj_entries[y][1] != '')
+			{
+				details__content.innerHTML += `<p class="details__content_p">${this_obj_entries[y][0]} <span> ${this_obj_entries[y][1]} </span></p>`;
+			}
+			else
+				{details__content.innerHTML += `<p class="details__content_p">${this_obj_entries[y][0]}</p>`;}
 		}
-		else if ( (car_nambers[i][y] > 10) && (car_nambers[i][y] < 100) )
+		details__content.innerHTML += `<div class="details__footer">всего ${item_count}</div>`;
+	}
+	container.innerHTML += `<p class="w100 p-h3">всего : ${all_cout} </p>`;
+}
+// постоянные
+function render_car_numbers( car, container )
+{
+	console.log('render_car_numbers( car, container )')
+	let car_entries = Object.entries(car);
+	console.dir(all_cars)
+	let car_entries_length = car_entries.length;
+	let all_cout = 0;
+	let this_details, this_obj_entries, this_obj_entries_len;
+	let details__content;
+	container.innerHTML = 'car_nambers';
+	for ( let i = 0; i < car_entries_length; i++ )
+	{
+		console.log('---')
+		console.log('Number : '+ car_entries[i][0] )
+		console.dir(car_entries[i][1])
+		container.innerHTML += `<details class="details" name="all" data-number="${car_entries[i][0]}">
+		<summary class="details__summary">
+		<span>${car_entries[i][0]}</span></summary>
+		<div class="details__content" data-namber="${car_entries[i][0]}"></div>
+		</details>`;
+	}
+	for ( let i = 0; i < car_entries_length; i++ )
+	{
+		this_details =  container.querySelector(`details[data-number="${car_entries[i][0]}"]`);
+		details__content = this_details.querySelector('.details__content');
+		this_obj_entries = Object.entries(car_entries[i][1]);
+		this_obj_entries_len = this_obj_entries.length;
+		details__content.innerHTML = '';
+		let item_count = 0;
+		for ( let y = 0; y < this_obj_entries_len; y++ )
 		{
-			details__content.innerHTML += `<p class="details__content_p"><span>0${car_nambers[i][y]}</span></p>`;
+			item_count++;
+			all_cout++;
+			if ( this_obj_entries[y][1] != '')
+			{
+				details__content.innerHTML += `<p class="details__content_p">${this_obj_entries[y][0]} <span> ${this_obj_entries[y][1]} </span></p>`;
+			}
+			else
+				{details__content.innerHTML += `<p class="details__content_p">${this_obj_entries[y][0]}</p>`;}
 		}
-		else
+		details__content.innerHTML += `<div class="details__footer">всего ${item_count}</div>`;
+	}
+	container.innerHTML += `<p class="w100 p-h3">всего : ${all_cout} </p>`;
+}
+function render_car_parking_by_time( car, container )
+{
+	console.log('render_car_parking_by_time( car )')
+	let car_entries = Object.entries(car);
+	let car_entries_length = car_entries.length;
+	let all_cout = 0;
+	// console.dir(car_entries)
+	// console.log(container)
+	container.innerHTML = '';
+	for ( let i = 0; i < car_entries_length; i++ )
+	{
+		// console.log('---')
+		// console.log('Number : '+ car_entries[i][0] )
+		// console.dir(car_entries[i][1])
+		container.innerHTML += `<details class="details" name="car_parking_by_time" data-number="${car_entries[i][0]}">
+		<summary class="details__summary">
+		<span>${car_entries[i][0]}</span></summary>
+		<div class="details__content" data-namber="${car_entries[i][0]}"></div>
+		</details>`;
+	}
+	let this_details, this_obj_entries, this_obj_entries_len;
+	let details__content;
+	for ( let i = 0; i < car_entries_length; i++ )
+	{
+		this_details =  container.querySelector(`details[data-number="${car_entries[i][0]}"]`);
+		details__content = this_details.querySelector('.details__content');
+		this_obj_entries = Object.entries(car_entries[i][1]);
+		this_obj_entries_len = this_obj_entries.length;
+		details__content.innerHTML = '';
+		let item_count = 0;
+		for ( let y = 0; y < this_obj_entries_len; y++ )
 		{
-			details__content.innerHTML += `<p class="details__content_p"><span>${car_nambers[i][y]}</span></p>`;
+			item_count++;
+			all_cout++;
+			details__content.innerHTML += `<p class="details__content_p">${this_obj_entries[y][0]} <span> ${this_obj_entries[y][1]} </span></p>`;
+		}
+		details__content.innerHTML += `<div class="details__footer">всего ${item_count}</div>`;
+	}
+	container.innerHTML += `<p class="w100 p-h3">всего : ${all_cout} </p>`;
+}
+// 
+function render_table_search( car, container )
+{
+	console.log('render_table_search()')
+	
+	console.log(container)
+	let car_entries = Object.entries(car);
+	let car_entries_length = car_entries.length;
+	let all_cout = 0;
+	let car_this_entries, car_this_entries_length = 0;
+	container.innerHTML = '';
+	console.dir(car_entries)
+	for ( let i = 0; i < car_entries_length; i++ )
+	{
+		//console.log('-'+i+'-')
+		//console.dir(car_entries[i][1])
+		car_this_entries = Object.entries(car_entries[i][1])
+		car_this_entries_length = car_this_entries.length;
+		//console.dir(car_this_entries)
+		container.innerHTML += `<tr class="table__tr_title">
+				<td>${i}</td>
+				<td>${i}</td>
+			<tr>`;
+		for ( let y = 0; y < car_this_entries_length; y++ )
+		{
+			//console.log('===')
+			//console.log('number : ' + car_this_entries[y][0] + ' description : ' + car_this_entries[y][1]);
+			container.innerHTML += `<tr>
+				<td>${car_this_entries[y][0]}</td>
+				<td>${car_this_entries[y][1]}</td>
+			<tr>`; 
 		}
 	}
-	count_all_cars += car_nambers[i].length;
-	details__content.innerHTML += `<p class="details__footer">всего - ${car_nambers[i].length}</p>`;
 }
-list_car_namber.innerHTML += `<p>всего машин: ${count_all_cars}</p>`
+// searh
+function search_nubber( form, container )
+{
+	let input_text = form.querySelector('.form__input-text[name="input-search-car"]')
+	let form__btn_reset = form.querySelector('.form__btn[type="reset"]')
+	let td_all;
+	let td_count = 0;
+	let search_check = false;
 
-// интерактивный
-input_car_interactive.addEventListener('input', (e) => {
-	input_car_interactive_number = input_car_interactive.value * 1;
-	car_search_out_text.dataset.show = '';
-	if ( (input_car_interactive.value.length >= 1) && input_car_interactive.value.length <= 3 )
-	{
-		car_search_out_text.dataset.show = '';
-		car_search_out_text.innerHTML = '';
-		if ( ( input_car_interactive_number < 1 ) || ( input_car_interactive_number > 999 )  )
+	form__btn_reset.addEventListener( 'click', () => {
+		input_text.value = '';
+	});
+	form.addEventListener('submit', (e) => {
+		e.preventDefault()
+		td_all = container.querySelectorAll('td');
+		td_count = td_all.length;
+		search_check = false;
+		input_text.style.color = '';
+		input_text.style.borderColor = '';
+		input_text.style.backgroundColor = '';
+		if ( input_text.value != '' )
 		{
-			car_search_out_text.innerHTML = '1 ... 999';
-			car_search_out_text.dataset.show = 'false';
-			input_car_interactive.value = '';
-		}
-		// search
-		for ( let i = 0; i < car_nambers.length; i++ )
-		{
-			empty_number = car_nambers[i].includes( input_car_interactive_number );
-			if ( empty_number )
+			for ( let i = 0; i < td_count; i++ )
 			{
-				car_search_out_text.innerHTML = input_car_interactive.value;
-				break;
+				td_all[i].style.backgroundColor = "";
+				td_all[i].style.color = "";
+				input_text.style.borderColor = '';
+
+				if ( td_all[i].textContent == input_text.value )
+				{
+					console.dir(td_all[i])
+					console.log('td_all[i].offsetTop - ' + td_all[i].offsetTop)
+					console.log('container.offsetTop - ' + container.offsetTop)
+					console.dir(container)
+					//td_all[i].offsetTop = container.offsetTop + 
+					// td_all[i].style.position = 'fixed';
+					// td_all[i].style.top = 150 + 'px'; 
+					// td_all[i].scrollIntoView( { behavior: "smooth", block: "end", inline: "nearest" } );
+					td_all[i].scrollIntoView( { behavior: "smooth", inline: "nearest" } );
+					search_check = true;
+					td_all[i].style.backgroundColor = "green";
+					td_all[i].style.color = "#fff";
+				}
+			}
+			if ( search_check == true )
+			{
+				input_text.style.borderColor = 'green';
+			}
+			else
+			{
+				input_text.style.borderColor = 'red';
+				input_text.style.backgroundColor = 'red';
+				input_text.style.color = '#fff';
 			}
 		}
-		if ( empty_number )
-		{
-			car_search_out_text.dataset.show = 'true';
-		}
-		else
-		{
-			car_search_out_text.dataset.show = 'false';
-		}
-	}
-	if ( input_car_interactive.value.length > 3 )
-	{
-		car_search_out_text.innerHTML = '1 ... 999';
-		car_search_out_text.dataset.show = 'false';
-		input_car_interactive.value = '';
-	}
-});
-
-car_search__form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	car_search__input_value = car_search__input.value * 1;
-	
-	if ( (car_search__input_value < 0 ) || (car_search__input_value > 999 ) )
-	{
-		car_search__out_car_number.dataset.show = 'false';
-		car_search__out_car_number.innerHTML = ' 0 ... 999';
-	}
-
-	for ( let i = 0; i < car_nambers.length; i++ )
-	{
-		empty_number = car_nambers[i].includes( car_search__input_value );
-		if ( empty_number )
-		{
-			break;
-		}
 		
-	}
-	if ( empty_number )
-	{
-		car_search__out_car_number.dataset.show = 'true';
-	}
-	else
-	{
-		car_search__out_car_number.dataset.show = 'false';
-	}
-});
-
+	});
+}
+// tab show
 tab__button_wrapper.addEventListener('click', (e) => {
 	for ( let i = 0; i < tab_btns_len; i++ )
 	{
